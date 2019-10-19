e241af75e6ecf108f82a56d0f33e64e9f2460a29
ZEYINGXU <37647493+ZEYINGXU@users.noreply.github.com>
2019-10-19 14:57:19 +1100

    version_1.4
    Optimize front page, static page

--- src/main/java/com/watermelon/jtarget/config/DataSourceConfig.java
+++ src/main/java/com/watermelon/jtarget/config/DataSourceConfig.java
@@ -1,18 +1,38 @@
 package com.watermelon.jtarget.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean(name = "dataSource")
     public DataSource initDataSource() {
-        return DataSourceBuilder.create().build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
     }
 
-
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        return sessionFactory;
+    }
 }
